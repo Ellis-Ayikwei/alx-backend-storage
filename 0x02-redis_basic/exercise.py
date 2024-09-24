58fcdf758cf910e520b8a35413dad4a633251318
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-24 17:45:07 +0000

    moved the key up abpve the wraps

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,11 +14,12 @@ def count_calls(method: Callable) -> Callable:
     Returns:
         A decorated method that increments a Redis key for each call
     """
+    key = f"{method.__qualname__}"
     @wraps(method)
     def wrapper(self, *args: Any, **kwargs: Any) -> Any:
         """Increment the count for that key every time the method is called"""
         if hasattr(self, '_redis'):
-            key = f"{method.__qualname__}"
+            
             self._redis.incr(key)
         return method(self, *args, **kwargs)
     return wrapper
