58973273160f2cea73800291f1ce40633199752e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-24 23:58:53 +0000

    the replay

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """ Main file """
 
+from exercise import replay
+
+
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
 s1 = cache.store("first")
 print(s1)
-s2 = cache.store("secont")
+s2 = cache.store("second")
 print(s2)
 s3 = cache.store("third")
 print(s3)
@@ -15,5 +18,10 @@ print(s3)
 inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
 outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
 
-print("inputs: {}".format(inputs))
+print("inputs: {}".format((inputs)))
 print("outputs: {}".format(outputs))
+zipped = zip(inputs, outputs)
+
+
+
+replay(cache.store)
