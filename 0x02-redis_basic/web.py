d84c762f0bb0deda2f95c81284814ffbc5f0b948
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 16:38:11 +0000

    reomoved the caching line

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,24 +14,20 @@ def method_call_count(method: Callable) -> Callable:
     def wrapper(url: str, *args, **kwargs) -> str:
         """Increment the count for that URL each time the method is called."""
         key = f"count:{url}"
-        ris.incr(key)  # Increments the key, creating it if it doesn't exist
+        ris.incr(key)
+        cache_key = f"cache:{url}"
+        content = method(url, *args, **kwargs)
+        ris.setex(cache_key, 10, content)
         return method(url, *args, **kwargs)
     return wrapper
 
 @method_call_count
 def get_page(url: str) -> str:
     """Fetch the page content, cache it, and track URL access count."""
-    cache_key = f"cache:{url}"
-    
-    # Check if the content is already cached
-    cached_content = ris.get(cache_key)
-    if cached_content:
-        return cached_content.decode('utf-8')
-    
-    # If not cached, fetch the content and cache it for 10 seconds
     response = requests.get(url)
     content = response.text
-    ris.setex(cache_key, 10, content)
+   
+    return content
    
 
 get_page("http://slowwly.robertomurray.co.uk")
