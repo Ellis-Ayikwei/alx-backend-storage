7ab7f0677cbc9b80f3f83fc3fad95d378708cf37
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-03 12:55:59 +0000

    te sort on avg

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,5 +5,15 @@
 
 
 def top_students(mongo_collection):
-    """returns all students sorted by average score"""
-    return mongo_collection.find({"averageScore": {"$gt": 70}})
+    """Return all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ])
