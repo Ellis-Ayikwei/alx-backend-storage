2cdcb16f4cd5739ea690cb8705441130e964009c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-03 11:18:59 +0000

    added the python3

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ the funtion to list all from a document """
 
 
 def list_all(mongo_collection):
     """the function to list all documents in a collection"""
     result = list(mongo_collection.find())
+    print(result)
     return result if result else []
