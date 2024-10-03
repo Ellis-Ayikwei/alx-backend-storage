f5f6227c6200a34e869ffda73454f8e70e86ac23
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-03 11:26:50 +0000

    Refactor list_all function to return all documents in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,5 @@
 
 
 def list_all(mongo_collection):
-    """the function to list all documents in a collection"""
-    result = list(mongo_collection.find())
-    print(result)
-    return result if result else []
+    """Return all documents in a collection"""
+    return [doc for doc in mongo_collection.find()]
